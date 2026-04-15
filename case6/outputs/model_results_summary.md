8398fb1d5909010ac30ccb1cd511e604c1668406
Uderwood-TZ <a1434916588@outlook.com>
2026-04-15 18:53:58 +0800

    Add case6 via Git LFS

---
+++ case6/outputs/model_results_summary.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a26b3ebce986bdb7b9501f04ceeeef863c0fdd5b8997c376335a34c477a2e9e2
+size 737
---
+++ case6/physics_core_fixed.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:95b4fb3684a130ca42f962ece1d83fac97b25c46777b4d86ee8aef5016395bd0
+size 20676
